bfa25c0762e19809584173dfba5be0480b513b93
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2018-06-12 20:51:05 +0800

    data aug train fin, release#2

--- 10_Name_Card_Detection/pytorch-faster-rcnn/tools/half_convert.py
+++ 10_Name_Card_Detection/pytorch-faster-rcnn/tools/half_convert.py
@@ -1,13 +1,13 @@
+import _init_paths
 from nets.resnet_v1 import resnetv1
 import torch
+import os
 
 net = resnetv1(num_layers=101)
 net.create_architecture(4, tag='default', anchor_scales=[8, 16, 32])
 
-net.load_state_dict(torch.load(os.path.join('../output', 'res101', 'NameCardtrainvalNameCardReal', 'default',
-                              'res101_faster_rcnn_iter_200000.pth'), map_location=lambda storage, loc: storage))
-			      net.eval()
-			      half_net = net.to(torch.float16)
-
-			      torch.save(half_net.state_dict(), 'params.pth')
-
+net.load_state_dict(torch.load(os.path.join('../output', 'res101', 'NameCardtrainvalNameCardFake', 'default',
+                              'res101_faster_rcnn_iter_250000.pth'), map_location=lambda storage, loc: storage))
+net.eval()
+half_net = net.to(torch.float16)
+torch.save(half_net.state_dict(), 'params.pth')
